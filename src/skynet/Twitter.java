129579a61e6f2ee6d28817c9f131b390ec671687
LegitMikeyG <michael.goolsbey@gmail.com>
2015-09-21 13:46:21 -0500

    Added some stuff
    stuff

--- src/skynet/Twitter.java
+++ src/skynet/Twitter.java
@@ -1,6 +1,28 @@
 package skynet;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.OAuthAuthorization;
+import twitter4j.auth.RequestToken;
+import twitter4j.User;
 
-public class Twitter {
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.media.ImageUpload;
+import twitter4j.media.ImageUploadFactory;
+import twitter4j.media.MediaProvider;
+
+import java.io.File;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Twitter 
+{
 	
 		//Insert page source for twitter.com here
+	
 }
