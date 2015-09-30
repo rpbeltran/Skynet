aedfd27006f3f6ef75fc6add66c7ee54182e9698
Ryan Beltran <rp.beltran@yahoo.com>
2015-09-29 20:36:54 -0500

    Cleaning up

--- src/skynet/Twitter.java
+++ src/skynet/Twitter.java
@@ -1,22 +1,12 @@
 package skynet;
-import twitter4j.Status;
-import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
 import twitter4j.auth.OAuthAuthorization;
-import twitter4j.auth.RequestToken;
-import twitter4j.User;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 import twitter4j.media.ImageUpload;
 import twitter4j.media.ImageUploadFactory;
-import twitter4j.media.MediaProvider;
 
 import java.io.File;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public final class twitter {
 	
