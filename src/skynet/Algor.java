b55e2034feb1adf6a4b913469449ba99840c5207
Ryan Beltran <rp.beltran@yahoo.com>
2015-09-29 20:58:19 -0500

    More Cleanup

--- src/skynet/Algor.java
+++ src/skynet/Algor.java
@@ -4,8 +4,8 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Algor {
-	
+public class Algor 
+{	
 	public static int[] scaleByBrightness(int[] rgb){
 		int sum = (rgb[0]+rgb[1]+rgb[2])/3;
 		return new int[] {sum,255-sum,0};
@@ -18,29 +18,25 @@ public class Algor {
 	public static NewImage basicIterative(BufferedImage image){
 		ExistingImage test = new ExistingImage(image);
 		NewImage newTest = new NewImage(test.getWidth(),test.getHeight());
-		for(int row = 0; row < test.getHeight();row ++){
-				for (int col = 0; col < test.getWidth(); col++){
-					newTest.setAt(col,row,Algor.scaleByBrightness(test.getRGBat(col, row)) );
-				
-			}
-		}
+		for(int row = 0; row < test.getHeight();row ++)
+			for (int col = 0; col < test.getWidth(); col++)
+				newTest.setAt(col,row,Algor.scaleByBrightness(test.getRGBat(col, row)) );
 		return newTest;
 	}
 	
 	public static int average(BufferedImage image){
 		List<Integer> averages = new ArrayList<Integer>();
 		ExistingImage test = new ExistingImage(image);
-		NewImage newTest = new NewImage(test.getWidth(),test.getHeight());
-		for(int row = 0; row < test.getHeight();row ++){
-				for (int col = 0; col < test.getWidth(); col++){
-					int[]rgb = test.getRGBat(col, row);
-					averages.add((rgb[0]+rgb[1]+rgb[2])/3);
+		for(int row = 0; row < test.getHeight();row ++)
+			for (int col = 0; col < test.getWidth(); col++)
+			{
+				int[]rgb = test.getRGBat(col, row);
+				averages.add((rgb[0]+rgb[1]+rgb[2])/3);
 			}
-		}
 		int av = 0;
-		for (int a:averages){
+		for (int a:averages)
 			av+=a;
-		} av/= averages.size();
+		av/= averages.size();
 		return av;
 	}
 
