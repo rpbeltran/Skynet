0dd9413d00e192a22f6cdc220f05058bde52b1f1
Ryan Beltran <rp.beltran@yahoo.com>
2015-09-29 20:34:19 -0500

    Forgot to add this file

--- src/skynet/Skynet.java
+++ src/skynet/Skynet.java
@@ -1,14 +1,23 @@
 package skynet;
 
+import java.awt.image.BufferedImage;
+
+import twitter4j.TwitterException;
+
 public class Skynet {
 
 	public static void main(String[] args) {
 		
-		Algor.basicIterativeTo(Camera.capture(), "output");
+		BufferedImage image = Camera.capture();
+		Algor.basicIterativeTo(image, "output");
+		int avg = Algor.average(image);
+		twitter tweeter = new twitter();
+		try {
+			tweeter.postImageWithMessage("output.png", "Hello astronomers, the average skyglow is currently "+avg);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
 		
-		
-		
-
 	}
 
 }
